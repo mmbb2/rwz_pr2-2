bde3191794f3f6839450573823e8d3595964b2a2
Maxym Tsymbalystyi <bakumax125@gmail.com>
2021-12-03 20:00:44 +0200

    4.3

--- js/index.js
+++ js/index.js
@@ -9,6 +9,12 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurants = document.querySelector(".cards-restaurants");
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector(".restaurants");
+const menu = document.querySelector(".menu");
+const logo = document.querySelector(".logo");
+const cardsMenu = document.querySelector(".cards-menu");
 
 let login = localStorage.getItem("login");
 
@@ -34,8 +40,6 @@ function ToggleModalAuth() {
       }
 }
 
-
-console.log(modalAuth)
 
 function notAuthorized() {
     console.log("Не авторизован");
@@ -100,6 +104,110 @@ function notAuthorized() {
     }
   }
 
+  function createCardReustarants() {
+    const card = `
+        <a class="card card-restaurant wow animate__animated animate__fadeInUp" data-wow-delay="0">
+            <img src="img/pizza-plus.png" alt="image" class="card-image">
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">
+                        Пицца плюс
+                    </h3>
+                    <span class="card-tag tag">
+                        50 мин
+                    </span>
+                </div>
+                <div class="card-info">
+                    <div class="rating">
+                        <img src="img/star.svg" alt="star" class="rating-star">
+                        4.5
+                    </div>
+                    <div class="price">
+                        От 900 ₽
+                    </div>
+                    <div class="category">
+                        Пицца
+                    </div>
+                </div>
+            </div>
+    </a>
+    `
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+function createCardGood() {
+    const card = document.createElement('div');
+    card.className = 'card wow fadeInUp';
+    card.setAttribute('data-wow-delay', 0);
+    card.insertAdjacentHTML('beforeend', `
+
+    <img src="img/image.png" alt="image" class="card-image">
+    <div class="card-text">
+        <div class="card-heading">
+            <h3 class="card-title card-title-regular">
+                Ролл угорь стандарт
+            </h3>
+        </div>
+        <div class="card-info">
+            <div class="ingredients">
+                Рис, угорь, соус унаги, кунжут, водоросли нори.
+            </div>
+        </div>
+        <div class="card-buttons">
+            <button class="button button-primary">
+                <span class="button-card-text">
+                    В корзину
+                </span>
+                <img src="img/shopping2.svg" alt="shopping-cart" class="button-card-image">
+            </button>
+            <strong class="card-price-bold">250 ₽</strong>
+        </div>
+    </div>
+    `);
+    cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {
+    if(login){
+        const target = event.target;
+    
+        const restourant = target.closest('.card-restaurant');
+    
+        cardsMenu.textContent = '';
+        
+        if (restourant) {
+            containerPromo.classList.add('hide');
+            restaurants.classList.add('hide');
+            menu.classList.remove('hide');
+    
+            createCardGood();
+            createCardGood();
+            createCardGood();
+            createCardGood();
+        }
+    }
+    else {
+      ToggleModalAuth();
+    }
+}
+
+function closeGoods() {
+    containerPromo.classList.remove('hidden');
+    restaurants.classList.remove('hidden');
+    menu.classList.add('hidden');
+}
+
+cardsRestaurants.addEventListener('click', openGoods);
+logo.addEventListener('click', function() {
+    closeGoods();
+})
+
   checkAuth();
+
+  createCardReustarants();
+createCardReustarants();
+createCardReustarants();
+createCardReustarants();
+
 
 new WOW().init();
